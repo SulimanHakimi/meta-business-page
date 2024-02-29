9091981dd8ce03664c74ac07f249b784442fd049
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-02-28 20:56:08 -0800

    finish html all parts

---
+++ guidanceData.js
@@ -0,0 +1,17 @@
+const guidanceData = {
+  title: "Guidance",
+  items: [
+    {
+      id: 1,
+      title: "Guidance",
+      description: "Get more profile visits on Instagram",
+      img:""
+    },
+    {
+      id: 2,
+      title: "Support",
+      description: "Troubleshoot a Rejected Ad",
+      img:""
+    },
+  ],
+};
